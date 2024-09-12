08284001926f793be7045646365355ed1b1846b4
AhmadMuzayyin <muzayyin654@gmail.com>
2024-09-12 12:21:09 +0700

    Create master crud

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\TahunAkademikController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('login');
 });
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [App\Http\Controllers\Home\HomeController::class, 'root'])->name('home');
